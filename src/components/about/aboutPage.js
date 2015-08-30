488ea1b4c6d4354044ff44806d1b9b0c42ca078d
Bohdan Shtepan <winsatana@ya.ru>
2015-08-30 04:43:44 +0300

    Adds AboutPage component

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,24 @@
+var React = require('react'),
+    About = Reqct.createClass({
+        render: function() {
+            return (
+                <div>
+                    <h1>About</h1>
+                    <p>
+                        This application uses following technologies:
+                        <ul>
+                            <li>React</li>
+                            <li>React Router</li>
+                            <li>Flux</li>
+                            <li>Node</li>
+                            <li>Gulp</li>
+                            <li>Browserify</li>
+                            <li>Bootstrap</li>
+                        </ul>
+                    </p>
+                </div>
+            );
+        }
+    });
+
+module.exports = About;
