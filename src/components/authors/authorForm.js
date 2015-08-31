68f9aa241f950eb9cf112f160c89322f8c88e0df
Bohdan Shtepan <winsatana@ya.ru>
2015-08-31 06:23:44 +0300

    Controlled Components

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -7,11 +7,13 @@ var React = require('react'),
                 <form>
                     <h1>Manage Author</h1>
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" className="form-control" placeholder="First Name" ref="firstName" value="" />
+                    <input type="text" className="form-control" placeholder="First Name" ref="firstName"
+                        value={this.props.author.firstName} onChange={this.props.onChange} />
                     <br/>
 
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" className="form-control" placeholder="Last Name" ref="lastName" value="" />
+                    <input type="text" className="form-control" placeholder="Last Name" ref="lastName"
+                        value={this.props.author.lastName} onChange={this.props.onChange} />
                     <br/>
 
                     <input type="submit" value="Save" className="btn btn-default" />
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -3,9 +3,28 @@
 var React = require('react'),
     AuthorForm = require('./authorForm'),
     ManageAuthorPage = React.createClass({
+        getInitialState: function() {
+            return {
+                author: {
+                    id: '',
+                    firstName: '',
+                    lastName: ''
+                }
+            };
+        },
+
+        setAuthorState: function(ev) {
+            var field = ev.target.name,
+                val = ev.target.value;
+
+            this.state.author[field] = val;
+
+            return this.setState({ author: this.state.author });
+        },
+
         render: function() {
             return (
-                <AuthorForm />
+                <AuthorForm author={this.state.author} onChange={this.setAuthorState} />
             );
         }
     });
