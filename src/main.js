b487e824cf307f4e9fca68909c75b1f3ec13d444
Bohdan Shtepan <winsatana@ya.ru>
2015-08-31 06:04:44 +0300

    Location

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ var React = require('react'),
     Router = require('react-router'),
     routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
     React.render(<Handler />, document.getElementById('app'));
 });
