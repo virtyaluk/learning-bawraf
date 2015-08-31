6036aa1629013fedc065d1f50a3c35d9572ca218
Bohdan Shtepan <winsatana@ya.ru>
2015-08-31 05:54:44 +0300

    Redirects

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,16 @@ var React = require('react'),
     DefaultRoute = Router.DefaultRoute,
     Route = Router.Route,
     NotFoundRoute = Router.NotFoundRoute,
+    Redirect = Router.Redirect,
     routes = (
         <Route name="app" path="/" handler={require('./components/app')}>
             <DefaultRoute handler={require('./components/homePage')} />
             <Route name="authors" handler={require('./components/authors/authorPage')} />
             <Route name="about" handler={require('./components/about/aboutPage')} />
             <NotFoundRoute handler={require('./components/notFoundPage')} />
+            <Redirect from="about-us" to="about" />
+            <Redirect from="awthors" to="authors" />
+            <Redirect from="about/*" to="about" />
         </Route>
     );
 
