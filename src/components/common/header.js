b712007b3893499bb4cd8d665baa8cb762c22902
Bohdan Shtepan <winsatana@ya.ru>
2015-08-31 05:46:44 +0300

    Links

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,18 +1,19 @@
 'use strict';
 
 var React = require('react'),
+    Link = require('react-router').Link,
     Header = React.createClass({
         render: function () {
             return (
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
-                        <a href="/" className="navbar-brand">
+                        <Link to="app" className="navbar-brand">
                             <img src="images/pluralsight-logo.png" />
-                        </a>
+                        </Link>
                         <ul className="nav navbar-nav">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/#authors">Authors</a></li>
-                            <li><a href="/#about">About</a></li>
+                            <li><Link to="app">Home</Link></li>
+                            <li><Link to="authors">Authors</Link></li>
+                            <li><Link to="about">About</Link></li>
                         </ul>
                     </div>
                 </nav>
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var React = require('react'),
+    Link = require('react-router').Link,
     Home = React.createClass({
         render: function() {
             return (
                 <div className="jumbotron">
                     <h1>Pluralsight Administration</h1>
                     <p>React, React Router, and Flux for ultra-responsive web apps.</p>
+                    <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
                 </div>
             );
         }
