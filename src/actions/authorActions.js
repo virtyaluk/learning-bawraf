49fd1e29190cf57a2784852c145bc159384f4f22
Bohdan Shtepan <winsatana@ya.ru>
2015-09-02 07:25:44 +0300

    Actions

---
+++ src/actions/authorActions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Dispatcher = require('../dispatcher/appDispatcher'),
+    authorApi = require('../api/authorApi'),
+    actionTypes = require('../constants/actionTypes'),
+    authorActions = {
+        createAuthro: function(author) {
+            var newAuthor = authorApi.saveAuthor(author);
+
+            Dispatcher.dispatch({
+                actionType: actionTypes.CREATE_AUTHOR,
+                author: newAuthor 
+            })
+        }
+    };
+
+module.exports = authorActions;
